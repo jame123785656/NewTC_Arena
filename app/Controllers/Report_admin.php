e0721ac11081c420d683e525273e8415505fc9fd
bew32 <tawatninja425@gmail.com>
2022-05-05 20:40:34 +0700

    dashboard

--- app/Controllers/Report_admin.php
+++ app/Controllers/Report_admin.php
@@ -1,9 +1,39 @@
 <?php namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use App\Models\UserModel;
+use App\Models\BookingModel;
+use App\Models\DetailModel;
 
 class report_admin extends Controller {
     public function Report_admin() {
-        echo view('report_admin');
+        $data = [];
+        $model_user = new UserModel();
+        $data['count_id'] = $model_user->total();
+        $model_booking = new BookingModel();
+        $data['book_total'] = $model_booking->totals();
+        $bookingModel = new BookingModel();
+        $data['books'] = $bookingModel->bookinglist();
+        $DetailModel = new DetailModel();
+        $data['detail'] = $DetailModel->join('Time','detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
+        echo view('report_admin', $data);
     }
+
+    // public function pass() {
+    //     $model = new passModel();
+    //     if($this->request->getGet('q')){
+    //         $q = $this->request->getGet('q');
+    //          $data['users'] = $model->like('booking', $q)->getAll();
+    //     }else {
+    //         $data['users'] = $model->getAll();
+    //     }
+    //     return view('report_admin, $data');
+    //     }
+
+    public function Search() {
+        $this->db->select("*");  
+        $this->db->like('booking',$this->input->get('search'));
+        $query = $this->db->get("booking"); 
+        return $query->result();
+     }
 }
--- app/Models/BookingModel.php
+++ app/Models/BookingModel.php
@@ -9,4 +9,28 @@ class BookingModel extends Model
     protected $table = 'booking';
     protected $primaryKey = 'B_id';
     protected $allowedFields = ['B_id','B_idUser','B_idFeld','B_day','B_img','B_status'];
+
+    public function totals() {
+        $data = $this->db
+            ->table('booking')
+            ->where('B_status', 3)
+            ->countAllResults();
+        return $data;
+    }
+
+    public function bookinglist() {
+        $data = $this->db
+            ->table('booking')
+            ->join('detail','booking.B_id = detail.d_id')
+            ->join('time','detail.d_id = time.t_id')
+            ->join('statuss','booking.B_status = statuss.S_id')
+            ->join('field','booking.B_idFeld = field.F_ID')
+            ->join('user','user.ID = booking.B_idUser')
+            ->groupBy('B_id')
+            ->where('B_status', 3)
+            ->orderBy('B_id', 'Asc')
+            ->get()
+            ->getResultArray();
+        return $data;
+    }
 }
