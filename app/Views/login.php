26c72e172573f26d3f73c5a27c1c62a7fe824614
PL-O <80671477+PL-O@users.noreply.github.com>
2022-05-17 15:25:27 +0700

    update,bookingpage

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +7,6 @@
     <link rel= "stylesheet" type= "text/css" href= "<?php echo base_url('./css/login.css'); ?>" >
     <title>เข้าสู่ระบบ</title>
 </head>
-
-
 <body>
 
     <div class="areaBackground">
@@ -45,5 +42,4 @@
         </div>
     </div>
 </body>
-
 </html>
