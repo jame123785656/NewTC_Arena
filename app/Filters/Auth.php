205e3308142c54d7f04ef3ed0c0f07ae63329a9d
jame123785656 <61955070+jame123785656@users.noreply.github.com>
2022-07-01 00:34:04 +0700

    ตกแต่งไม่มีสิทธิ์ถ้าไม่เข้าสู่ระบบ

--- app/Filters/Auth.php
+++ app/Filters/Auth.php
@@ -8,6 +8,10 @@ class Auth implements FilterInterface {
     public function before(RequestInterface $request, $arguments = null) {
         // if user not logged in
         if (!session()->get('logged_in')) {
+            $session = session();
+            $session->setFlashdata('swel_title', 'กรุณาเข้าสู่ระบบก่อน');
+            $session->setFlashdata('swel_icon', 'error');
+            $session->setFlashdata('swel_button', 'ตกลง');
             return redirect()->to('/login');
         }
     }
