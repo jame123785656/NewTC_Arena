3349f2e25f63c891548af9697d8d6f68c93f61b4
jame123785656 <61955070+jame123785656@users.noreply.github.com>
2022-07-28 00:33:16 +0700

    เพิ่มการนับสมาชิกmemberว่ามีกี่คน

--- app/Views/manageuser.php
+++ app/Views/manageuser.php
@@ -7,48 +7,61 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- CSS only -->
-  <link rel= "stylesheet" type= "text/css" href= "<?php echo base_url('./css/employee.css'); ?>" >
-  <link rel= "stylesheet" type= "text/css" href= "<?php echo base_url('./css/manager.css'); ?>" >
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('./css/employee.css'); ?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('./css/manager.css'); ?>">
   <title>จัดการผู้ใช้</title>
 </head>
-<body>
-<?php require('component/navbar_admin.php') ?>
-    
-    <center>
-        <h1>จัดการผู้ใช้</h1>
-    <center>
-    <hr width="50%">
-    <br>
-    <div class="table-style">
-    <table style="width:100%">
-     <tr>
-        <td bgcolor="#DDD">ชื่อลูกค้า</td>
-        <td bgcolor="#DDD">ชื่อ-นามสกุล</td>
-        <td bgcolor="#DDD">อีเมล์</td>
-        <td bgcolor="#DDD">เบอร์โทรศัพท์</td>
-        <td bgcolor="#DDD">สถานะ </td>
-      </tr>
-      
-      <?php if ($user) : ?>
-    <?php foreach ($user as $user) : ?>
-     <tr>
-        <td><?php echo $user['username']?></td>
-        <td><?php echo $user['name']?></td>
-        <td><?php echo $user['Email']?></td>
-        <td><?php echo $user['phone']?></td>
-        <td class= <?php if($user['status']==='admin'){
-                    echo 'red';
-                  } else if($user['status']==='member'){
-                    echo 'Green';
-                   } ?>><?php echo $user['status']?><a href="/historyadmin/<?php echo $user['ID']?>"> <img src="/image/iconeyes.png" width="15px" height="15px" align="right"></a></td>
-      </tr>
-      <?php endforeach; ?>
-<?php endif; ?>
-    </table>
-</div>
 
-</center>
-    
-  </body>
+<body>
+  <?php require('component/navbar_admin.php') ?>
+
+  <center>
+    <h1>จัดการผู้ใช้</h1>
+    <center>
+      <hr width="50%">
+      <br>
+        <p class="num-user">
+          <?php
+          $count = 0;
+          if ($user) : ?>
+            <?php foreach ($user as $users) : ?>
+              <?php if ($users['status'] === 'member') {
+                $count += 1;
+              } ?>
+            <?php endforeach; ?>
+          <?php endif; ?>
+          จำนวนผู้ใช้ <?php echo $count ?> คน
+        </p>
+      <div class="table-style">
+        <table style="width:100%">
+          <tr>
+            <td bgcolor="#DDD">ชื่อลูกค้า</td>
+            <td bgcolor="#DDD">ชื่อ-นามสกุล</td>
+            <td bgcolor="#DDD">อีเมล์</td>
+            <td bgcolor="#DDD">เบอร์โทรศัพท์</td>
+            <td bgcolor="#DDD">สถานะ </td>
+          </tr>
+
+          <?php if ($user) : ?>
+            <?php foreach ($user as $user) : ?>
+              <tr>
+                <td><?php echo $user['username'] ?></td>
+                <td><?php echo $user['name'] ?></td>
+                <td><?php echo $user['Email'] ?></td>
+                <td><?php echo $user['phone'] ?></td>
+                <td class=<?php if ($user['status'] === 'admin') {
+                            echo 'red';
+                          } else if ($user['status'] === 'member') {
+                            echo 'Green';
+                          } ?>><?php echo $user['status'] ?><a href="/historyadmin/<?php echo $user['ID'] ?>"> <img src="/image/iconeyes.png" width="15px" height="15px" align="right"></a></td>
+              </tr>
+            <?php endforeach; ?>
+          <?php endif; ?>
+        </table>
+      </div>
+
+    </center>
+
+</body>
 
 </html>
