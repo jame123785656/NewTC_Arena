18f5a3ab870fb7c4eb63f84e4334e41e3e7268fe
jame123785656 <61955070+jame123785656@users.noreply.github.com>
2022-07-28 00:35:43 +0700

    มีการตั้งค่าตัวแปร+มีการตัดยอดให้แสดงในแต่ละวันและแสดงทั้งหมด

--- app/Controllers/Report_admin.php
+++ app/Controllers/Report_admin.php
@@ -1,36 +1,45 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 use CodeIgniter\Controller;
 use App\Models\UserModel;
 use App\Models\BookingModel;
 use App\Models\DetailModel;
 
-class report_admin extends Controller {
-    public function Report_admin() {
-        $data = [];
+class report_admin extends Controller
+{
+    public function Report_admin()
+    {
         $model_user = new UserModel();
         $data['count_id'] = $model_user->total();
         $model_booking = new BookingModel();
-        $data['book_total'] = $model_booking->totals();
-        $data_ptotal = $model_booking->ytotal();
-        foreach($data_ptotal as $v){
+        $data['book_total'] = $model_booking->totals(3);
+        $data_ptotal = $model_booking->ytotal(3);
+        foreach ($data_ptotal as $v) {
             $data['book_price'] = $v['sumprice'];
         }
-        $data_mtotal = $model_booking->mtotal();
-        foreach($data_mtotal as $v){
+        $data_mtotal = $model_booking->mtotal(3);
+        foreach ($data_mtotal as $v) {
             $sumprice_y = $v['sumprice'];
-            if($sumprice_y == 0 || $sumprice_y == null) {
+            if ($sumprice_y == 0 || $sumprice_y == null) {
                 $data['book_month'] = "0";
-            }else {
+            } else {
                 $data['book_month'] = $sumprice_y;
             }
         }
-        $data['books'] = $model_booking->bookinglist();
-        $data['books_wait'] = $model_booking->bookinglist_wait();
+        $data['books'] = $model_booking->bookinglist(3);
+        $data['books_wait'] = $model_booking->bookinglist_wait(2);
         $DetailModel = new DetailModel();
-        $data['detail'] = $DetailModel->join('Time','detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
+        $data['detail'] = $DetailModel->join('Time', 'detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
         echo view('report_admin', $data);
     }
-
-
+    public function Report_All()
+    {
+        $model_booking = new BookingModel();
+        $data['booking'] = $model_booking->bookingall(3);
+        $DetailModel = new DetailModel();
+        $data['detail'] = $DetailModel->join('Time', 'detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
+        echo view('report_all',$data);
+    }
 }
