886591e8f0b4f86ac0b9e2eb8fc39c5bf94b2386
jame123785656 <61955070+jame123785656@users.noreply.github.com>
2022-11-01 16:58:02 +0700

    เพิ่มหน้า

--- app/Controllers/Historyadmin.php
+++ app/Controllers/Historyadmin.php
@@ -2,6 +2,9 @@
 use CodeIgniter\Controller;
 use App\Models\BookingModel;
 use App\Models\DetailModel;
+use App\Models\UserModel;
+
+
 
 class Historyadmin extends Controller
 {
@@ -10,8 +13,46 @@ class Historyadmin extends Controller
         helper(['form']);
         $BookingModel = new BookingModel();
         $DetailModel = new DetailModel();
-        $data['booking'] = $BookingModel->join('field','booking.B_idFeld = field.F_ID')->join('statuss','booking.B_status = statuss.S_id')->where('B_idUser',$ID)->orderBy('B_id', 'Asc')->findAll();
+        $UserModel = new UserModel();
+        $data['user'] = $UserModel->where('ID',$ID)->first();
+        $data['booking'] = $BookingModel->join('field','booking.B_idFeld = field.F_ID')->join('statuss','booking.B_status = statuss.S_id')->join('user','booking.B_idUser = user.ID')->where('B_idUser',$ID)->orderBy('B_id', 'Asc')->findAll();
         $data['detail'] = $DetailModel->join('Time','detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
         echo view('historyadmin',$data);
+    }
+    public function historycancel($ID = null)
+    {
+        helper(['form']);
+        $BookingModel = new BookingModel();
+        $DetailModel = new DetailModel();
+        $UserModel = new UserModel();
+        $sql = "S_id IN (4,7,8)";
+        $data['user'] = $UserModel->where('ID',$ID)->first();
+        $data['booking'] = $BookingModel->join('field','booking.B_idFeld = field.F_ID')->join('statuss','booking.B_status = statuss.S_id')->join('user','booking.B_idUser = user.ID')->where('B_idUser',$ID)->where($sql)->orderBy('B_id', 'Asc')->findAll();
+        $data['detail'] = $DetailModel->join('Time','detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
+        echo view('historycancel',$data);
+    }
+    public function historysucceed($ID = null)
+    {
+        helper(['form']);
+        $BookingModel = new BookingModel();
+        $DetailModel = new DetailModel();
+        $UserModel = new UserModel();
+        $sql = "S_id IN (3)";
+        $data['user'] = $UserModel->where('ID',$ID)->first();
+        $data['booking'] = $BookingModel->join('field','booking.B_idFeld = field.F_ID')->join('statuss','booking.B_status = statuss.S_id')->join('user','booking.B_idUser = user.ID')->where('B_idUser',$ID)->where($sql)->orderBy('B_id', 'Asc')->findAll();
+        $data['detail'] = $DetailModel->join('Time','detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
+        echo view('historysucceed',$data);
+    }
+    public function historypending($ID = null)
+    {
+        helper(['form']);
+        $BookingModel = new BookingModel();
+        $DetailModel = new DetailModel();
+        $UserModel = new UserModel();
+        $sql = "S_id IN (2)";
+        $data['user'] = $UserModel->where('ID',$ID)->first();
+        $data['booking'] = $BookingModel->join('field','booking.B_idFeld = field.F_ID')->join('statuss','booking.B_status = statuss.S_id')->join('user','booking.B_idUser = user.ID')->where('B_idUser',$ID)->where($sql)->orderBy('B_id', 'Asc')->findAll();
+        $data['detail'] = $DetailModel->join('Time','detail.t_id = time.T_id')->orderBy('d_id', 'Asc')->findAll();
+        echo view('historypending',$data);
     }
 }
